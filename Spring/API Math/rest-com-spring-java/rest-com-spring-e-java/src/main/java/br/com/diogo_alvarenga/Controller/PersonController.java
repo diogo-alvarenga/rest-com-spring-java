5e33a056522661908eb79db37f52b669118bf072
diogo <alvarwngadiogo228@gmail.com>
2025-10-14 16:32:16 -0700

    commit

--- Spring/API Math/rest-com-spring-java/rest-com-spring-e-java/src/main/java/br/com/diogo_alvarenga/Controller/PersonController.java
+++ Spring/API Math/rest-com-spring-java/rest-com-spring-e-java/src/main/java/br/com/diogo_alvarenga/Controller/PersonController.java
@@ -1,8 +1,11 @@
 package br.com.diogo_alvarenga.Controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,11 +21,47 @@ public class PersonController {
 	private PersonService service;
 	
 	
+	@RequestMapping(
+			method = RequestMethod.GET,
+			produces = MediaType.APPLICATION_JSON_VALUE)
+	public List<Person> findById() {
+		return service.findAll();
+	}
+
+	
 	@RequestMapping(value = "/{id}",
 			method = RequestMethod.GET,
 			produces = MediaType.APPLICATION_JSON_VALUE)
 	public Person findById(@PathVariable("id") String id) {
 		return service.findById(id);
+	}
+	
+	
+	@RequestMapping(
+			method = RequestMethod.POST,
+			consumes = MediaType.APPLICATION_JSON_VALUE,//irá consumir
+			produces = MediaType.APPLICATION_JSON_VALUE//irá produzir
+			)
+	public Person create (@RequestBody Person person) {//o parametro vem do body e nao do path
+		return service.create(person);
+	}
+	
+	@RequestMapping(
+			method = RequestMethod.PUT,
+			consumes = MediaType.APPLICATION_JSON_VALUE,
+			produces = MediaType.APPLICATION_JSON_VALUE
+			)
+	public Person updatePerson(@RequestBody Person person) {
+		return service.update(person);
+	}
+	
+	
+	@RequestMapping(
+			value= "/{id}",
+			method = RequestMethod.DELETE
+			)
+	public void delete(@PathVariable("id") String id) {
+		service.delete(id);
 	}
 
 }
--- Spring/API Math/rest-com-spring-java/rest-com-spring-e-java/src/main/java/br/com/diogo_alvarenga/Service/PersonService.java
+++ Spring/API Math/rest-com-spring-java/rest-com-spring-e-java/src/main/java/br/com/diogo_alvarenga/Service/PersonService.java
@@ -1,5 +1,7 @@
 package br.com.diogo_alvarenga.Service;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
 import java.util.logging.Logger;
 
@@ -28,5 +30,47 @@ public class PersonService {
 		person.setGender("Male");
 		return person;
 
+	}
+	
+	public List<Person> findAll() {
+		logger.info("Find all People!");
+		List<Person> persons = new ArrayList<Person>();
+		
+		for(int i =0;i<9;i++) {
+			Person person = mockPerson(i);
+			persons.add(person);
+		}
+		
+		return persons;
+	}
+
+	public Person create(Person person) {
+		logger.info("Create one Person!");
+		
+		return person;
+	}
+	
+	public Person update(Person person) {
+		logger.info("Updating one Person!");
+			
+		return person;
+	}
+	
+	public void delete(String id) {
+		logger.info("Deleting one Person!");
+
+	}
+	
+	
+	private Person mockPerson(int i) {
+		
+		Person person = new Person();
+		person.setId(counter.incrementAndGet());
+		person.setFirstName("Firstname "+i);
+		person.setLastName("Lastname "+i);
+		person.setAddress("Some Address in Brasil");
+		person.setGender("Male");
+		
+		return person;
 	}
 }
