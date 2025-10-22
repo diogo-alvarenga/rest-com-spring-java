d35f47a0247c505e5a9b8fe8395c0097fd77c320
diogo <alvarwngadiogo228@gmail.com>
2025-10-22 15:46:39 -0700

    Commit

---
+++ Spring/Trabalhando com Logs/rest-com-spring-java/rest-com-spring-e-java/src/main/java/br/com/diogo_alvarenga/Service/PersonService.java
@@ -0,0 +1,76 @@
+package br.com.diogo_alvarenga.Service;
+
+import java.util.List;
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+
+import br.com.diogo_alvarenga.Exception.ResourceNotFoundException;
+import br.com.diogo_alvarenga.Model.Person;
+import br.com.diogo_alvarenga.Repository.PersonRepository;
+
+@Service
+public class PersonService {
+
+	private final AtomicLong counter = new AtomicLong();
+
+	@Autowired
+	PersonRepository repository;
+	
+	//Logger será explicado mais tarde
+	private org.slf4j.Logger logger = org.slf4j.LoggerFactory.getLogger(PersonService.class.getName());
+	
+	
+	public Person findById(Long id) {
+		logger.info("Finding one Person!");
+		
+		return repository.findById(id).orElseThrow(() -> new ResourceNotFoundException("No records found for this ID"));
+
+	}
+	
+	
+	
+	public List<Person> findAll() {
+		
+		logger.info("Find all People!");
+
+		return repository.findAll();
+	}
+	
+	
+
+	public Person create(Person person) {
+		logger.info("Create one Person!");
+		
+		return repository.save(person);
+	}
+	
+	
+	
+	public Person update(Person person) {
+		logger.info("Updating one Person!");
+			
+		Person entity = repository.findById(person.getId())
+				.orElseThrow(() -> new ResourceNotFoundException("No records found for this ID"));
+		entity.setFirstName(person.getFirstName());
+		entity.setLastName(person.getLastName());
+		entity.setAddress(person.getAddress());
+		entity.setGender(person.getGender());
+		
+		return repository.save(entity);
+	}
+	
+	
+	
+	public void delete(Long id) {
+		logger.info("Deleting one Person!");
+
+		Person entity = repository.findById(id)
+				.orElseThrow(()-> new ResourceNotFoundException("No records found for this ID"));
+		repository.delete(entity);
+	}
+	
+	
+}
